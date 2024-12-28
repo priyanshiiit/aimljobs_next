88211a4194b063720a8e71b180097e1ea6bfdddb
priyanshiiit <priyanshiiit@gmail.com>
2024-12-28 16:52:00 +0530

    adds analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Toaster position="top-center" />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
